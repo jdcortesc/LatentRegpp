374cc430331eee7c7a9e6a8edbe19fc8ad51beba
Juan David Cortés <jdcortesc@unal.edu.co>
2019-06-11 17:26:40 -0500

    Delete rr.R

--- rr.R
+++
@@ -1,9 +0,0 @@
-rr=function(pt.cuad,w.cuad,betas,pats,ncatg,freqs,npats,dat.dic,pats.cod){
-
-pii=posteriori(pt.cuad = pt.cuad,w.cuad = w.cuad,betas = betas,pats = pats,ncatg = ncatg,npats,dat.dic)
-freq.matrix=matrix(freqs,nrow=length(pt.cuad),ncol=length(freqs),byrow=T)
-(pii*freq.matrix)%*%pats.cod
-
-}
-
-#sum(rr(pt.cuad = pt.cuad,w.cuad = w.cuad,betas = betas,pats = pats,ncatg = ncatg,freqs = freqs,npats=npats,dat.dic=dat.dic,pats.cod=pats.cod))
